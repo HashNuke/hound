9da3785d10b1122059ad13ae3b88d93c60d5e734
Akash Manohar J <akash@akash.im>
2013-12-18 08:12:09 +0530

    Import modules into json driver

--- lib/hound/json_driver.ex
+++ lib/hound/json_driver.ex
@@ -1,2 +1,12 @@
 defmodule Hound.JsonDriver do
+
+  import Hound.JsonDriver.Session
+  import Hound.JsonDriver.Page
+  import Hound.JsonDriver.Navigation
+  import Hound.JsonDriver.Element
+  import Hound.JsonDriver.ScriptExecution
+  import Hound.JsonDriver.Window
+  import Hound.JsonDriver.Screenshot
+  import Hound.JsonDriver.Dialog
+
 end
