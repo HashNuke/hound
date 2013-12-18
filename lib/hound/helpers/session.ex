2c6b8fa313665f963812f5200cd2a0c7b375ea54
Akash Manohar J <akash@akash.im>
2013-12-18 08:25:30 +0530

    Fix session helpers

--- lib/hound/helpers/session.ex
+++ lib/hound/helpers/session.ex
@@ -2,17 +2,25 @@ defmodule Hound.Helpers.Session do
 
   @doc "Get capabilities of a particular session"
   defmacro session do
-    quote do: session(var!(meta[:session_id]))
+    quote do
+      session(var!(meta[:hound_connection]), var!(meta[:hound_session_id]))
+    end
   end
+
 
   @doc "Delete a session"
   defmacro delete_session do
-    quote do: delete_session(var!(meta[:session_id]))
+    quote do
+      delete_session(var!(meta[:hound_connection]), var!(meta[:hound_session_id]))
+    end
   end
+
 
   @doc "Set the timeout for a particular type of operation"
   defmacro set_timeout(operation, time) do
-    quote do: set_timeout(var!(meta[:session_id]), operation, time)
+    quote do
+      set_timeout(var!(meta[:hound_connection]), var!(meta[:hound_session_id]), operation, time)
+    end
   end
 
 end
