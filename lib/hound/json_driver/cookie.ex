e2de7002188da41d0f4169abb429a64c9099b160
Akash Manohar J <akash@akash.im>
2013-12-20 18:01:32 +0530

    Complete cookie calls

--- lib/hound/json_driver/cookie.ex
+++ lib/hound/json_driver/cookie.ex
@@ -1,16 +1,31 @@
 defmodule Hound.JsonDriver.Cookie do
   import Hound.JsonDriver.Utils
 
-#TODO
-#   @doc "Get cookies"
-#   defcallback cookies(session_id :: String.t)
+  @doc "Get cookies"
+  @spec cookies() :: List.t
+  def cookies() do
+    session_id = Hound.get_current_session_id
+    make_req(:get, "session/#{session_id}/cookie")
+  end
 
-#   @doc "Set cookie"
-#   defcallback set_cookie(session_id :: String.t, cookie)
+  @doc "Set cookie"
+  @spec set_cookie(String.t) :: :ok
+  def set_cookie(cookie) do
+    session_id = Hound.get_current_session_id
+    make_req(:post, "session/#{session_id}/cookie", [cookie: cookie])
+  end
 
-#   @doc "Delete all cookies for the current page"
-#   defcallback delete_cookies(session_id :: String.t)
+  @doc "Delete all cookies"
+  @spec delete_cookies() :: :ok
+  def delete_cookies() do
+    session_id = Hound.get_current_session_id
+    make_req(:delete, "session/#{session_id}/cookie")
+  end
 
-#   @doc "Delete cookie"
-#   defcallback delete_cookie(session_id :: String.t, name)
+  @doc "Delete a cookie with the given name"
+  @spec delete_cookie(String.t) :: :ok
+  def delete_cookie(name) do
+    session_id = Hound.get_current_session_id
+    make_req(:delete, "session/#{session_id}/cookie/#{name}")
+  end
 end
