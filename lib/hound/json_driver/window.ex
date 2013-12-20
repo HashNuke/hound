ab72dd3d4d59e9d2932542e006b7a2c49d4b570a
Akash Manohar J <akash@akash.im>
2013-12-20 18:03:24 +0530

    Fixed code in comments

--- lib/hound/json_driver/window.ex
+++ lib/hound/json_driver/window.ex
@@ -15,37 +15,38 @@ defmodule Hound.JsonDriver.Window do
     make_req(:get, "session/#{session_id}/window_handles")
   end
 
+
 # TODO
 #   @doc "Change focus to frame"
-#   @spec focus_frame(Dict.t, String.t, String.t) :: :ok
-#   def focus_frame(connection, session_id :: String.t, frame_id) do
+#   @spec focus_frame(String.t) :: :ok
+#   def focus_frame(frame_id) do
 #   end
 
 #   @doc "Change focus to another window"
-#   def focus_window(connection, session_id :: String.t, window_identifier) do
+#   def focus_window(window_id) do
 #   end
 
 #   @doc "Close current window"
-#   def close_current_window(connection, session_id :: String.t) do
+#   def close_current_window() do
 #   end
 
 #   @doc "Change size of window"
-#   def set_window_size(connection, session_id :: String.t, window_handle, size) do
+#   def set_window_size(window_handle, size) do
 #   end
 
 #   @doc "Get window size"
-#   def window_size(connection, session_id :: String.t, window_handle) do
+#   def window_size(window_handle) do
 #   end
 
 #   @doc "Get window position"
-#   def window_position(connection, session_id :: String.t, window_handle) do
+#   def window_position(window_handle) do
 #   end
 
 #   @doc "Set window position"
-#   def set_window_position(connection, session_id :: String.t, window_handle, position) do
+#   def set_window_position(window_handle, position) do
 #   end
 
 #   @doc "Maximize window"
-#   def set_window_position(connection, session_id :: String.t, window_handle) do
+#   def maximize_window(window_handle) do
 #   end
 end
