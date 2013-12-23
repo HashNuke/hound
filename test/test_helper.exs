2a2fe25099be33296333210745148bb358c5af77
Akash Manohar J <akash@akash.im>
2013-12-24 00:23:33 +0530

    Fix test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,19 +1,17 @@
-ExUnit.start [autorun: false, max_cases: 5]
+test_server_config = [
+  port: 9090,
+  server_name: 'hound_test_server',
+  server_root: Path.absname('test/sample_pages'),
+  document_root: Path.absname('test/sample_pages'),
+  bind_address: 'localhost'
+]
+
+{:ok, pid} = :inets.start(:httpd, test_server_config)
+
 Hound.start
 
 System.at_exit fn(_exit_status) ->
-  test_server_config = [
-    port: 9090,
-    server_name: 'hound_test_server',
-    server_root: Path.absname('test/sample_pages'),
-    document_root: Path.absname('test/sample_pages'),
-    bind_address: 'localhost'
-  ]
-
-  {:ok, pid} = :inets.start(:httpd, test_server_config)
-
-  test_result = ExUnit.run
-
   :ok = :inets.stop(:httpd, pid)
-  test_result
 end
+
+ExUnit.start [max_cases: 5]
