a75a0dc0049f9c338e78b938806616ed7201d533
Akash Manohar J <akash@akash.im>
2013-12-23 23:19:12 +0530

    Return element IDs or error

--- lib/hound/json_driver/page.ex
+++ lib/hound/json_driver/page.ex
@@ -19,40 +19,73 @@ defmodule Hound.JsonDriver.Page do
   @spec find_element(String.t, String.t) :: Dict.t
   def find_element(strategy, selector) do
     session_id = Hound.get_current_session_id
-    params = [using: Hound.InternalHelper.selector_strategy(strategy), value: selector]
-    make_req(:post, "session/#{session_id}/element", params)
+    params = [using: Hound.InternalHelpers.selector_strategy(strategy), value: selector]
+    case make_req(:post, "session/#{session_id}/element", params) do
+      [{"ELEMENT", element_id}] ->
+        element_id
+      value ->
+        value
+    end
   end
 
   @doc "Find elements on current page"
   @spec find_all_elements(atom, String.t) :: List.t
   def find_all_elements(strategy, selector) do
     session_id = Hound.get_current_session_id
-    params = [using: Hound.InternalHelper.selector_strategy(strategy), value: selector]
-    make_req(:post, "session/#{session_id}/elements", params)
+    params = [using: Hound.InternalHelpers.selector_strategy(strategy), value: selector]
+    case make_req(:post, "session/#{session_id}/elements", params) do
+      {:error, value} ->
+        {:error, value}
+      elements ->
+        lc [{"ELEMENT", element_id}] inlist elements do
+          element_id
+        end
+    end
   end
+
 
   @doc "Find element within element"
   @spec find_within_element(String.t, atom,String.t) :: Dict.t
   def find_within_element(element_id, strategy, selector) do
     session_id = Hound.get_current_session_id
-    params = [using: Hound.InternalHelper.selector_strategy(strategy), value: selector]
-    make_req(:post, "session/#{session_id}/element/#{element_id}/element", params)
+    params = [using: Hound.InternalHelpers.selector_strategy(strategy), value: selector]
+    case make_req(:post, "session/#{session_id}/element/#{element_id}/element", params) do
+      [{"ELEMENT", element_id}] ->
+        element_id
+      value ->
+        value
+    end
   end
+
 
   @doc "Find elements within element"
   @spec find_all_within_element(String.t, atom, String.t) :: List.t
   def find_all_within_element(element_id, strategy, selector) do
     session_id = Hound.get_current_session_id
-    params = [using: Hound.InternalHelper.selector_strategy(strategy), value: selector]
-    make_req(:post, "session/#{session_id}/element/#{element_id}/elements", params)
+    params = [using: Hound.InternalHelpers.selector_strategy(strategy), value: selector]
+    case make_req(:post, "session/#{session_id}/element/#{element_id}/elements", params) do
+      {:error, value} ->
+        {:error, value}
+      elements ->
+        lc [{"ELEMENT", element_id}] inlist elements do
+          element_id
+        end
+    end
   end
+
 
   @doc "Get element on page currently in focus"
   @spec element_in_focus() :: Dict.t
   def element_in_focus do
     session_id = Hound.get_current_session_id
-    make_req(:post, "session/#{session_id}/element/active")
+    case make_req(:post, "session/#{session_id}/element/active") do
+      [{"ELEMENT", element_id}] ->
+        element_id
+      value ->
+        value
+    end
   end
+
 
   @doc "Send sequence of key strokes to active element. The modifier keys are not released after this command is run."
   @spec send_keys(List.t) :: :ok
