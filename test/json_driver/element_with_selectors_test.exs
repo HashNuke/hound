bcc31ef47401863ad1b9fd7d682a71b236c4439f
Akash Manohar J <akash@akash.im>
2013-12-25 14:32:57 +0530

    Rename test files to end with 'Test' suffix

--- test/json_driver/element_with_selectors_test.exs
+++ test/json_driver/element_with_selectors_test.exs
@@ -1,16 +1,16 @@
-defmodule ElementTestWithSelectors do
+defmodule ElementWithSelectorsTest do
   use ExUnit.Case
   use Hound.Helpers
 
   hound_session
 
-  test "should get visible text of an element" do
+  test "should get visible text of an element, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     assert visible_text({:class, "example"}) == "Paragraph"
   end
 
 
-  test "should input value into field" do
+  test "should input value into field, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
 
     element = {:name, "username"}
@@ -23,7 +23,7 @@ defmodule ElementTestWithSelectors do
   end
 
 
-  test "should fill a field with a value" do
+  test "should fill a field with a value, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     element = {:name, "username"}
 
@@ -35,13 +35,13 @@ defmodule ElementTestWithSelectors do
   end
 
 
-  test "should get tag name of element" do
+  test "should get tag name of element, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     assert tag_name({:name, "username"}) == "input"
   end
 
 
-  test "should clear field" do
+  test "should clear field, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     element = {:name, "username"}
 
@@ -53,7 +53,7 @@ defmodule ElementTestWithSelectors do
   end
 
 
-  test "should return true if item is selected in a checkbox or radio" do
+  test "should return true if item is selected in a checkbox or radio, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     element = {:id, "speed-superpower"}
     click element
@@ -61,43 +61,43 @@ defmodule ElementTestWithSelectors do
   end
 
 
-  test "should return false if item is *not* selected" do
+  test "should return false if item is *not* selected, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     assert selected?({:id, "speed-flying"}) == false
   end
 
 
-  test "Should return true if element is enabled" do
+  test "Should return true if element is enabled, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     assert element_enabled?({:name, "username"}) == true
   end
 
 
-  test "Should return false if element is *not* enabled" do
+  test "Should return false if element is *not* enabled, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     assert element_enabled?({:name, "promocode"}) == false
   end
 
 
-  test "should get attribute value of an element" do
+  test "should get attribute value of an element, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     assert attribute_value({:class, "example"}, "data-greeting") == "hello"
   end
 
 
-  test "should return true if an element is displayed" do
+  test "should return true if an element is displayed, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     assert element_displayed?({:class, "example"})
   end
 
 
-  test "should return false if an element is *not* displayed" do
+  test "should return false if an element is *not* displayed, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     assert element_displayed?({:class, "hidden-element"}) == false
   end
 
 
-  test "should get an element's location on screen" do
+  test "should get an element's location on screen, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     {loc_x, loc_y} = element_location({:class, "example"})
     assert is_integer(loc_x) || is_float(loc_x)
@@ -105,27 +105,27 @@ defmodule ElementTestWithSelectors do
   end
 
 
-  test "should get an element's size" do
+  test "should get an element's size, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     size = element_size({:class, "example"})
     assert size == {400, 100}
   end
 
 
-  test "should get css property of an element" do
+  test "should get css property of an element, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     assert css_property({:class, "container"}, "display") == "block"
   end
 
 
-  test "should click on an element" do
+  test "should click on an element, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     click({:class, "submit-form"})
     assert current_url == "http://localhost:9090/page2.html"
   end
 
 
-  test "should submit a form element" do
+  test "should submit a form element, when selector is passed" do
     navigate_to "http://localhost:9090/page1.html"
     submit_element({:name, "username"})
     assert current_url == "http://localhost:9090/page2.html"
