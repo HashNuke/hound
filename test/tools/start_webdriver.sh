4d95fea850046831e8122d938e15749f7bc48c84
Akash Manohar J <akash@akash.im>
2014-01-01 00:01:55 +0530

    Install unzip along with chromium

--- test/tools/start_webdriver.sh
+++ test/tools/start_webdriver.sh
@@ -3,7 +3,7 @@ then
   "export DISPLAY=:99.0"
   "/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16"
     cd ~/src
-    sudo apt-get install chromium-browser
+    sudo aptitude -y -q install unzip chromium-browser
     wget http://chromedriver.storage.googleapis.com/2.8/chromedriver_linux64.zip
     unzip chromedriver_linux64.zip
     nohup /./$HOME/src/chromedriver &
