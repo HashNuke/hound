c16263fe679da4171445aa7b807cdd591faf378a
Akash Manohar J <akash@akash.im>
2013-12-31 23:07:17 +0530

    Add shell script to start whatever webdriver is relevant

---
+++ test/tools/start_webdriver.sh
@@ -0,0 +1,19 @@
+if [ $WEBDRIVER == "chrome_driver" ]
+then
+    sudo apt-get install chromium-browser
+    wget http://chromedriver.storage.googleapis.com/2.8/chromedriver_linux64.zip
+    unzip chromedriver_linux64.zip
+    nohup ./chromedriver &
+    echo "Running with ChromeDriver"
+elif [ $WEBDRIVER == "phantomjs" ]
+then
+    nohup phantomjs -w &
+    echo "Running with PhantomJs"
+elif [ $WEBDRIVER == "selenium" ]
+then
+  echo "Running with Selenium"
+  "export DISPLAY=:99.0"
+  "/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16"
+  wget https://selenium.googlecode.com/files/selenium-server-standalone-2.39.0.jar
+  nohup java -jar selenium-server-standalone-2.39.0.jar &
+fi
