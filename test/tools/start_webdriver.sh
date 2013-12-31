69e997d9067c7cdba33023840222464422e0f763
Akash Manohar J <akash@akash.im>
2013-12-31 23:49:24 +0530

    Start xvfb for chrome driver too

--- test/tools/start_webdriver.sh
+++ test/tools/start_webdriver.sh
@@ -1,5 +1,7 @@
 if [[ $WEBDRIVER == "chrome_driver" ]]
 then
+  "export DISPLAY=:99.0"
+  "/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16"
     cd ~/src
     sudo apt-get install chromium-browser
     wget http://chromedriver.storage.googleapis.com/2.8/chromedriver_linux64.zip
