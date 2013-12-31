75bb52ccf8732cba2bbc1a8845aae3bb4a39b4d1
Akash Manohar J <akash@akash.im>
2013-12-31 17:53:15 +0530

    Update version to 0.5
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Hound.Mixfile do
 
   def project do
     [ app: :hound,
-      version: "0.0.1",
+      version: "0.5",
       elixir: ">= 0.12.0",
       deps: deps,
       source_url: "http://github.com/HashNuke/hound",
