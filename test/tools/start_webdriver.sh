2d970db7cd4ca7a31a5c96c2cb1f7d798b7fd903
Akash Manohar J <akash@akash.im>
2013-12-31 23:31:07 +0530

    cd into the right dir to download tools

--- test/tools/start_webdriver.sh
+++ test/tools/start_webdriver.sh
@@ -1,19 +1,21 @@
 if [[ $WEBDRIVER == "chrome_driver" ]]
 then
-    # sudo apt-get install chromium-browser
-    # wget http://chromedriver.storage.googleapis.com/2.8/chromedriver_linux64.zip
-    # unzip chromedriver_linux64.zip
-    /./$HOME/src/chromedriver
-    echo "Running with ChromeDriver"
+    cd ~/src
+    sudo apt-get install chromium-browser
+    wget http://chromedriver.storage.googleapis.com/2.8/chromedriver_linux64.zip
+    unzip chromedriver_linux64.zip
+    nohup /./$HOME/src/chromedriver &
+    echo "Running with ChromeDriver..."
 elif [[ $WEBDRIVER == "phantomjs" ]]
 then
     nohup phantomjs -w &
-    echo "Running with PhantomJs"
+    echo "Running with PhantomJs..."
 elif [[ $WEBDRIVER == "selenium" ]]
 then
-  echo "Running with Selenium"
   "export DISPLAY=:99.0"
   "/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16"
+  cd ~/src
   wget https://selenium.googlecode.com/files/selenium-server-standalone-2.39.0.jar
   nohup java -jar selenium-server-standalone-2.39.0.jar &
+  echo "Running with Selenium..."
 fi
