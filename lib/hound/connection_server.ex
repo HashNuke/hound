6ae8f05936f0ad5cd24318e8dc0522acf80c54bf
Akash Manohar J <akash@akash.im>
2013-12-31 21:35:50 +0530

    Also assume default browser names if not passed

--- lib/hound/connection_server.ex
+++ lib/hound/connection_server.ex
@@ -5,25 +5,26 @@ defmodule Hound.ConnectionServer do
 
   def start_link(options // []) do
     driver = options[:driver] || :selenium
-    {default_port, path_prefix} = case driver do
+
+    {default_port, default_path_prefix, default_browser} = case driver do
       :chrome_driver ->
-        {9515, nil}
+        {9515, nil, "chrome"}
       :phantomjs ->
-        {8910, "wd/hub"}
+        {8910, nil, "phantomjs"}
       _ -> # assume selenium
-        {4444, "wd/hub"}
+        {4444, "wd/hub", "firefox"}
     end
 
     driver_info = [
       driver: driver,
+      browser: options[:browser]  || default_browser,
       type: options[:driver_type] || Hound.JsonDriver,
       host: options[:host] || "http://localhost",
       port: options[:port] || default_port,
-      path_prefix: options[:path_prefix]
+      path_prefix: options[:path_prefix] || default_path_prefix
     ]
 
     state = [sessions: [], driver_info: driver_info]
-
     :gen_server.start_link({ :local, :hound_connection }, __MODULE__, state, [])
   end
 
