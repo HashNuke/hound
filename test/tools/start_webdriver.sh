cc74ee9ad0f8a90391bfb72e2f982de67b941c55
Akash Manohar J <akash@akash.im>
2014-06-07 18:36:31 +0530

    Start phantomjs in port 9090

--- test/tools/start_webdriver.sh
+++ test/tools/start_webdriver.sh
@@ -1,6 +1,6 @@
 if [[ $WEBDRIVER == "phantomjs" ]]
 then
-    nohup phantomjs -w &
+    nohup phantomjs --webdriver=9090 &
     echo "Running with PhantomJs..."
 elif [[ $WEBDRIVER == "selenium" ]]
 then
