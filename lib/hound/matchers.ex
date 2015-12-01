2df7b3d46259710201a9461e6d03a1e5ed2ba6fa
Akash Manohar J <akash@akash.im>
2015-12-01 16:04:26 +0530

    Update text matchers

--- lib/hound/matchers.ex
+++ lib/hound/matchers.ex
@@ -2,52 +2,53 @@ defmodule Hound.Matchers do
   @moduledoc "Text and element matchers"
 
   import Hound.Helpers.Page
+  import Hound.Helpers.Element
 
-  @type text :: String.t
-  @type selector :: String.t
+  @type element_selector :: {atom, String.t}
+  @type element :: element_selector | String.t
 
 
   @doc """
   Returns true if text is found on the page.
 
-  visible_text?("Paragraph")
-  visible_text?("Paragraph")
+  visible_in_page?(~r/Paragraph/)
 
   The text is matched case-sensitive.
   """
-  @spec visible_text?(text) :: Boolean.t
-  def visible_text?(text) do
-    xpath_string = query(text)
-    element_ids  = find_all_elements(:xpath, xpath_string)
-    element_ids != []
+  @spec visible_in_page?(Regex.t) :: Boolean.t
+  def visible_in_page?(pattern) do
+    inner_text = {:tag, "body"}
+    |> attribute_value("innerText")
+
+    Regex.match?(pattern, inner_text)
   end
 
 
   @doc """
   Returns true if text is found on the page inside an element.
+  Only id and class selectors are supported.
 
-  visible_text_within?({:class, "block"}, "Paragraph")
-  visible_text_within?({:id, "foo"}, "Paragraph")
+  visible_in_element?({:class, "block"}, ~r/Paragraph/)
+  visible_in_element?({:id, "foo"}, ~r/paragraph/iu)
 
-  The text is matched case-sensitive.
+  The text is matched case-sensitive
   """
-  @spec visible_text_within?(selector, text) :: Boolean.t
-  def visible_text_within?(selector, text) do
-    xpath_string = query(text, selector)
-    element_ids  = find_all_elements(:xpath, xpath_string)
-    element_ids != []
+  @spec visible_in_element?(element, Regex.t) :: Boolean.t
+  def visible_in_element?(selector, pattern) do
+    inner_text = attribute_value(selector, "innerText")
+    Regex.match?(pattern, inner_text)
   end
 
 
-  defp query(text) do
+  defp xpath_query(text) do
     "//*[contains(text(), '#{text}') or @value = '#{text}']"
   end
 
-  defp query(text, {:class, selector}) do
+  defp xpath_query({:class, selector}, text) do
     "//*[contains(concat(' ', normalize-space(@class), ' '), ' #{selector} ')]//*[contains(text(), '#{text}')]"
   end
 
-  defp query(text, {:id, selector}) do
+  defp xpath_query({:id, selector}, text) do
     "//*[@id='#{selector}' and contains(text(), '#{text}')]"
   end
 end
