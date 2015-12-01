54841f3ff224a28b96a5fbab1a249230b4bf7486
Akash Manohar J <akash@akash.im>
2015-12-01 12:00:38 +0530

    Empty matchers module

--- lib/hound/matchers.ex
+++ lib/hound/matchers.ex
@@ -1,10 +1,3 @@
 defmodule Hound.Matchers do
   @moduledoc false
-
-  defmacro __using__([]) do
-    quote do
-      import Hound.Matchers.Text
-      import unquote(__MODULE__)
-    end
-  end
 end
