d85abe1a985267fae7cfe36862d24fe1c955c5e7
Akash Manohar J <akash@akash.im>
2015-12-07 08:38:14 +0530

    Increase selenium wait time to 12 seconds

--- test/tools/start_webdriver.sh
+++ test/tools/start_webdriver.sh
@@ -10,5 +10,5 @@ elif [ "$WEBDRIVER" = "selenium" ]; then
   wget http://selenium-release.storage.googleapis.com/2.45/selenium-server-standalone-2.48.2.jar
   nohup java -jar selenium-server-standalone-2.48.2.jar &
   echo "Running with Selenium..."
-  sleep 10
+  sleep 12
 fi
