a5020e4385519bc6486a7b6f452cb94d288fc939
Akash Manohar J <akash@akash.im>
2015-12-18 09:18:05 +0530

    selenium url fix

--- test/tools/start_webdriver.sh
+++ test/tools/start_webdriver.sh
@@ -7,7 +7,7 @@ if [ "$WEBDRIVER" = "phantomjs" ]; then
   sleep 3
 elif [ "$WEBDRIVER" = "selenium" ]; then
   mkdir -p $HOME/src
-  wget http://selenium-release.storage.googleapis.com/2.48.2/selenium-server-standalone-2.48.2.jar
+  wget http://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar
   nohup java -jar selenium-server-standalone-2.48.2.jar &
   echo "Running with Selenium..."
   sleep 10
